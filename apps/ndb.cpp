6aa1c333960650b04448a4492fb2aa4f4fbfba0e
LinckeTheo <lincketheo@gmail.com>
2025-03-12 22:37:46 -0400

    fixes test

--- apps/ndb.cpp
+++ apps/ndb.cpp
@@ -6,31 +6,33 @@
 int
 main ()
 {
-  u32 data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  usize nelem = sizeof(data) / sizeof(*data);
+  u32 data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  usize nelem = sizeof (data) / sizeof (*data);
 
-  int fd = open("test.bin", O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-  assert(fd != -1);
+  int fd = open ("test.bin", O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+  assert (fd != -1);
 
-  FileBuffer fb(U32, fd);
-  fb.append(data, nelem);
-  fb.append(data, nelem);
+  FileBuffer fb (U32, fd);
+  fb.append (data, nelem);
+  fb.append (data, nelem);
 
   // 1 2 3 4 5 6 7 8 9 10 1 2 3 4 5 6 7 8 9 10
 
   u32 dest[10];
 
-  result<usize> ret = fb.read(dest, 10, (srange){
-    .start = 1,
-    .end = 100,
-    .span = 4,
-  });
+  result<usize> ret = fb.read (dest, 10,
+                               (srange){
+                                   .start = 1,
+                                   .end = 100,
+                                   .span = 4,
+                               });
 
-  assert(ret.ok);
+  assert (ret.ok);
 
-  for(int i = 0; i < ret.value; ++i) {
-    printf("%u\n", dest[i]);
-  }
+  for (int i = 0; i < ret.value; ++i)
+    {
+      printf ("%u\n", dest[i]);
+    }
 
   return 0;
 }
--- include/Buffer.hpp
+++ include/Buffer.hpp
@@ -10,7 +10,8 @@ public:
 
   // Read [range] from source into [dest]
   // Does not change internal data
-  virtual result<usize> read (void *dest, usize dnelem, srange range) const = 0;
+  virtual result<usize> read (void *dest, usize dnelem, srange range) const
+      = 0;
 
   // Write [nelem] elements from [data] into source
   // Does not change internal data
