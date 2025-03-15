fc96044529e971e3f5436fcb66dd88b881c187ad
Theo Lincke <lincketheo@gmail.com>
2025-03-15 14:47:28 -0400

    fixes test

--- src/compiler/scanner.cpp
+++ src/compiler/scanner.cpp
@@ -1,4 +1,5 @@
 #include "compiler/scanner.hpp"
+#include "compiler/tokens.hpp"
 #include "macros.hpp"
 #include "testing.hpp"
 
@@ -16,7 +17,6 @@ public:
 private:
 #define scanner_assert()                                                      \
   assert (data);                                                              \
-  assert (dlen > 0);                                                          \
   assert (start <= current)
 
   bool is_end ();
@@ -42,15 +42,16 @@ scan (const char *data, usize dlen)
 {
   result<token_arr> arr = token_arr_create ();
   if (!arr.ok)
-    {
-      return err<token_arr> ();
-    }
+  {
+    return err<token_arr> ();
+  }
 
   Scanner s (data, dlen);
   if (!s.scan (&arr.value).ok)
-    {
-      return err<token_arr> ();
-    }
+  {
+    token_arr_free(&arr.value);
+    return err<token_arr> ();
+  }
 
   return ok (arr.value);
 }
@@ -70,31 +71,31 @@ Scanner::scan (token_arr *arr)
   token_arr_assert (arr);
 
   while (true)
-    {
-      // Scan next token
-      result<token> t = scan_next_token ();
-      if (!t.ok)
-        {
-          return err<void> ();
-        }
-
-      // Push next token
-      result<void> res = token_arr_push (arr, t.value);
-      if (!res.ok)
-        {
-          return err<void> ();
-        }
-
-      if (t.value.type == T_EOF)
-        {
-          break;
-        }
-    }
-
-  if (error)
+  {
+    // Scan next token
+    result<token> t = scan_next_token ();
+    if (!t.ok)
     {
       return err<void> ();
     }
+
+    // Push next token
+    result<void> res = token_arr_push (arr, t.value);
+    if (!res.ok)
+    {
+      return err<void> ();
+    }
+
+    if (t.value.type == T_EOF)
+    {
+      break;
+    }
+  }
+
+  if (error)
+  {
+    return err<void> ();
+  }
 
   return ok_void ();
 }
@@ -113,9 +114,9 @@ Scanner::peek_char ()
   scanner_assert ();
 
   if (is_end ())
-    {
-      return EOF;
-    }
+  {
+    return EOF;
+  }
   return data[current];
 }
 
@@ -134,27 +135,27 @@ Scanner::skip_whitespace ()
   scanner_assert ();
 
   while (true)
+  {
+    if (is_end ())
     {
-      if (is_end ())
+      return;
+    }
+
+    // Check next whitespace
+    char c = peek_char ();
+    switch (c)
+    {
+      case WHITESPACE_NL_C:
+        {
+          next_char (); // ignore return - we know it's just whitespace
+          break;
+        }
+      default:
         {
           return;
         }
-
-      // Check next whitespace
-      char c = peek_char ();
-      switch (c)
-        {
-        case WHITESPACE_NL_C:
-          {
-            next_char (); // ignore return - we know it's just whitespace
-            break;
-          }
-        default:
-          {
-            return;
-          }
-        }
     }
+  }
 }
 
 void
@@ -163,9 +164,9 @@ Scanner::scan_over_integer ()
   scanner_assert ();
 
   while (is_digit (peek_char ()))
-    {
-      next_char (); // ignore return - captured in final atoi
-    }
+  {
+    next_char (); // ignore return - captured in final atoi
+  }
 }
 
 static struct
@@ -174,7 +175,7 @@ static struct
   token_t t;
   usize slen;
 } keywords[] = {
-  { "write", T_WRITE, 5 },
+  { "WRITE", T_WRITE, 5 },
   { nullptr, T_EOF },
 };
 
@@ -186,17 +187,17 @@ Scanner::check_keyword ()
 
   usize dlen = current - start;
   for (int i = 0; keywords[i].str; ++i)
+  {
+    if (keywords[i].slen != dlen)
     {
-      if (keywords[i].slen != dlen)
-        {
-          continue;
-        }
-
-      if (strncmp (data, keywords[i].str, keywords[i].slen) == 0)
-        {
-          return ok (keywords[i].t);
-        }
+      continue;
     }
+
+    if (strncmp (data, keywords[i].str, keywords[i].slen) == 0)
+    {
+      return ok (keywords[i].t);
+    }
+  }
 
   return err<token_t> ();
 }
@@ -208,16 +209,16 @@ Scanner::parse_alpha ()
 
   // Scan over all alpha numeric chars
   while (is_alpha_num (peek_char ()))
-    {
-      next_char (); // ignore output - part of the string
-    }
+  {
+    next_char (); // ignore output - part of the string
+  }
 
   // Check if it's a keyword
   result<token_t> kw = check_keyword ();
   if (kw.ok)
-    {
-      return kw.value;
-    }
+  {
+    return kw.value;
+  }
 
   return T_IDENT;
 }
@@ -229,9 +230,9 @@ Scanner::scan_next_token ()
 
   result<token_t> t = scan_next_token_t ();
   if (!t.ok)
-    {
-      return err<token> ();
-    }
+  {
+    return err<token> ();
+  }
 
   return ok (token_create (&data[start], current - start, t.value, start));
 }
@@ -245,13 +246,13 @@ Scanner::scan_next_token_t ()
   start = current;
 
   if (is_end ())
-    {
-      return ok (T_EOF);
-    }
+  {
+    return ok (T_EOF);
+  }
 
   char c = next_char ();
   switch (c)
-    {
+  {
     case '(':
       {
         return ok (T_LEFT_PAREN);
@@ -275,31 +276,94 @@ Scanner::scan_next_token_t ()
     default:
       {
         if (is_digit (c))
-          {
-            scan_over_integer ();
-            return ok (T_INTEGER);
-          }
+        {
+          scan_over_integer ();
+          return ok (T_INTEGER);
+        }
         else if (is_alpha (c))
-          {
-            return ok (parse_alpha ());
-          }
+        {
+          return ok (parse_alpha ());
+        }
         else
-          {
-            return err<token_t> ();
-          }
+      {
+          return err<token_t> ();
+        }
       }
-    }
+  }
 }
 
-TEST (scan_tests)
+typedef struct {
+  token_t type;
+} expected_token;
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+void test_scan(const char *input,
+               expected_token expected[],
+               size_t expected_len) {
+
+  result<token_arr> t = scan(input, strlen(input));
+
+  test_assert_equal(t.ok, true, "%d");
+
+  size_t token_count = expected_len;
+  for (size_t i = 0; i < token_count; i++) {
+    test_assert_equal(t.value.tokens[i].type, expected[i].type, "%d");
+  }
+
+  token_arr_free(&t.value);
+}
+
+TEST(scan_tests)
 {
   const char *s1 = "WRITE 5 [a, (,,,(,bc,WRITE5,5WRITE,9]]),[)";
-  result<token_arr> t = scan (s1, strlen (s1));
+  expected_token expected1[] = {
+    {T_WRITE},
+    {T_INTEGER},
+    {T_LEFT_BRACKET},
+    {T_IDENT},
+    {T_COMMA},
+    {T_LEFT_PAREN},
+    {T_COMMA},
+    {T_COMMA},
+    {T_COMMA},
+    {T_LEFT_PAREN},
+    {T_COMMA},
+    {T_IDENT},
+    {T_COMMA},
+    {T_IDENT},
+    {T_COMMA},
+    {T_INTEGER},
+    {T_WRITE},
+    {T_COMMA},
+    {T_INTEGER},
+    {T_RIGHT_BRACKET},
+    {T_RIGHT_BRACKET},
+    {T_RIGHT_PAREN},
+    {T_COMMA},
+    {T_LEFT_BRACKET},
+    {T_RIGHT_PAREN},
+    {T_EOF}
+  };
+  test_scan(s1, expected1, ARRAY_SIZE(expected1));
 
-  test_assert_equal (t.ok, true, "%d");
+  const char *s2 = "";
+  expected_token expected2[] = { {T_EOF} };
+  test_scan(s2, expected2, ARRAY_SIZE(expected2));
 
-  if (t.ok)
-    {
-      token_arr_print (&t.value);
-    }
+  const char *s3 = "    \t  \n  ";
+  expected_token expected3[] = { {T_EOF} };
+  test_scan(s3, expected3, ARRAY_SIZE(expected3));
+
+  const char *s4 = "IDENTIFIER";
+  expected_token expected4[] = { {T_IDENT}, {T_EOF} };
+  test_scan(s4, expected4, ARRAY_SIZE(expected4));
+
+  const char *s5 = "[ ] ( ) ,";
+  expected_token expected5[] = { {T_LEFT_BRACKET}, {T_RIGHT_BRACKET}, {T_LEFT_PAREN}, {T_RIGHT_PAREN}, {T_COMMA}, {T_EOF} };
+  test_scan(s5, expected5, ARRAY_SIZE(expected5));
+
+  const char *s6 = "WRITE$ 123 abc!";
+  result<token_arr> t6 = scan(s6, strlen(s6));
+  test_assert_equal(t6.ok, false, "%d");
 }
--- src/compiler/tokens.cpp
+++ src/compiler/tokens.cpp
@@ -7,27 +7,27 @@
 #define token_assert(t) assert (t)
 
 #define __tttostr_case(t)                                                     \
-  case t:                                                                     \
-    return #t;
+case t:                                                                     \
+  return #t;
 
 #define ttforeach(func)                                                       \
-  func (T_WRITE);                                                             \
-  func (T_IDENT);                                                             \
-  func (T_INTEGER);                                                           \
-  func (T_LEFT_BRACKET);                                                      \
-  func (T_RIGHT_BRACKET);                                                     \
-  func (T_LEFT_PAREN);                                                        \
-  func (T_RIGHT_PAREN);                                                       \
-  func (T_COMMA);                                                             \
-  func (T_EOF);
+func (T_WRITE);                                                             \
+func (T_IDENT);                                                             \
+func (T_INTEGER);                                                           \
+func (T_LEFT_BRACKET);                                                      \
+func (T_RIGHT_BRACKET);                                                     \
+func (T_LEFT_PAREN);                                                        \
+func (T_RIGHT_PAREN);                                                       \
+func (T_COMMA);                                                             \
+func (T_EOF);
 
 static const char *
 tttostr (token_t t)
 {
   switch (t)
-    {
-      ttforeach (__tttostr_case);
-    }
+  {
+    ttforeach (__tttostr_case);
+  }
   return NULL;
 }
 
@@ -42,10 +42,10 @@ token_create (const char *loc, usize len, token_t type, int pos)
   };
 
   if (type == T_INTEGER)
-    {
-      // Implicit assert(ret.literal is integer) because we already scanned it
-      ret.int_val = atoi (ret.literal);
-    }
+  {
+    // Implicit assert(ret.literal is integer) because we already scanned it
+    ret.int_val = atoi (ret.literal);
+  }
 
   return ret;
 }
@@ -63,10 +63,10 @@ token_arr_print (token_arr *arr)
   token_arr_assert (arr);
 
   for (int i = 0; i < arr->len; ++i)
-    {
-      token t = arr->tokens[i];
-      token_print (&t);
-    }
+  {
+    token t = arr->tokens[i];
+    token_print (&t);
+  }
 }
 
 result<token_arr>
@@ -75,10 +75,10 @@ token_arr_create ()
   token_arr ret;
   ret.tokens = (token *)malloc (INITIAL_CAP * sizeof *ret.tokens);
   if (ret.tokens == nullptr)
-    {
-      perror ("malloc");
-      return err<token_arr> ();
-    }
+  {
+    perror ("malloc");
+    return err<token_arr> ();
+  }
   ret.len = 0;
   ret.cap = INITIAL_CAP;
   return ok (ret);
@@ -100,15 +100,15 @@ token_arr_push (token_arr *t, token tok)
 
   // TODO - this only needs 1 realloc
   while (t->len + 1 >= t->cap)
+  {
+    token *next = (token *)realloc (t->tokens, t->cap * 2 * sizeof*t->tokens);
+    if (next == nullptr)
     {
-      token *next = (token *)realloc (t->tokens, t->cap * 2);
-      if (next == nullptr)
-        {
-          return err<void> ();
-        }
-      t->tokens = next;
-      t->cap = t->cap * 2;
+      return err<void> ();
     }
+    t->tokens = next;
+    t->cap = t->cap * 2;
+  }
 
   t->tokens[t->len++] = tok;
 
